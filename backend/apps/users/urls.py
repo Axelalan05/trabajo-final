7bffbf2a2147e1af0157551827c1a84a24b3a752
Axelalan05 <axeldavidalan05@gmail.com>
2026-06-04 22:27:20 +0000

    login endpoint

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 from apps.users.views import RegisterView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('register/', views.RegisterView.as_view()),
+    path('login/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
 ]
